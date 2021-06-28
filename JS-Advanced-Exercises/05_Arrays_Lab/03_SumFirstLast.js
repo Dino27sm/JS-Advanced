424052d84e777506c66aeb1b8e761e936f3509ac
Mironov <mironov5712s@gmail.com>
2021-06-28 16:38:15 +0300

    Create 03_SumFirstLast.js

---
+++ JS-Advanced-Exercises/05_Arrays_Lab/03_SumFirstLast.js
@@ -0,0 +1,7 @@
+function sumFirstLast(inputArray){
+    let firstNum = Number(inputArray[0]);
+    let lastNum = Number(inputArray[inputArray.length - 1]);
+    console.log(firstNum + lastNum);
+}
+
+sumFirstLast(['5', '10']);
