590e531c754e5d939e1565da9e2466755fb4c2ec
Mironov <mironov5712s@gmail.com>
2021-06-29 07:40:10 +0300

    Create 01_ArrayWithDelimiter.js

---
+++ JS-Advanced-Exercises/06_Arrays_Exercise/01_ArrayWithDelimiter.js
@@ -0,0 +1,12 @@
+function arrayWithDelimiter(inpArr, delimiter){
+    return inpArr.join(delimiter);
+}
+
+console.log(arrayWithDelimiter(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+));
