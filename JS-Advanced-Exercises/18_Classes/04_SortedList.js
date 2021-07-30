d656b69bccf2853e50971ee48834e0a641430b1d
Mironov <mironov5712s@gmail.com>
2021-07-30 20:55:37 +0300

    Create 04_SortedList.js

---
+++ JS-Advanced-Exercises/18_Classes/04_SortedList.js
@@ -0,0 +1,27 @@
+class List {
+    constructor () {
+        this.list = []
+        this.size = this.list.length
+    }
+
+    updateSize = () => this.size = this.list.length
+    orderList = () => this.list.sort((a, b) => a - b)
+
+    add (e) {
+        this.list.push(e)
+        this.updateSize()
+        this.orderList()
+    }
+    remove (i) {
+        if (this.list[i] !== undefined) {
+            this.list.splice(i, 1)
+            this.updateSize()
+            this.orderList()
+        }
+    }
+    get (i) {
+        if (this.list[i] !== undefined) {
+            return this.list[i]
+        }
+    }
+}
